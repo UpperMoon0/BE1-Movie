f0b436f91b2855b1c1a4236167429a1951c2a238
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-06-18 14:54:48 +0700

    Added a method to create files

---
+++ src/main/java/util/FileManager.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.io.File;
+import java.io.IOException;
+
+import ui.UI;
+
+public class FileManager {
+    public static File createFile(String path) {
+        try {
+            File file = new File(path);
+            
+            // Create the parent directories if they don't exist
+            file.getParentFile().mkdirs();
+
+
+            // Create the file
+            if (file.createNewFile()) {
+                if (UI.getIsDebug()) 
+                    System.out.println("File created: " + file.getName());
+            } else {
+                if (UI.getIsDebug()) 
+                    System.out.println("User data file already exists.");
+            }
+
+            return file;
+        } catch (IOException e) {
+            if (UI.getIsDebug()) 
+                e.printStackTrace();          
+        }
+
+        return null;
+    }
+}
