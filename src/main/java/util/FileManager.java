1f8691d682ecd1d059da13aec3982ea0d8fe44c5
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-06-21 12:41:07 +0700

    FileManager changes

--- src/main/java/util/FileManager.java
+++ src/main/java/util/FileManager.java
@@ -10,15 +10,18 @@ import ui.UI;
 
 public class FileManager {
     // Create a file
-    public static File createFile(String path) {
+    public static File createFileIfNotExist(String path) {
         try {
             File file = new File(path);
             
-            // Create the parent directories if they don't exist
-            file.getParentFile().mkdirs();
+            // Create the file if it doesn't exist
+            if (!file.exists()) {
+                // Create the parent directories if they don't exist
+                file.getParentFile().mkdirs();
 
-            // Create the file
-            file.createNewFile();
+                // Create the file
+                file.createNewFile();
+            }
 
             return file;
         } catch (Exception e) {
@@ -32,11 +35,8 @@ public class FileManager {
     // Write to a file
     public static void writeToFile(String path, String str) {
         try {
-            File file = new File(path);   
-            
             // Create the file if it doesn't exist
-            if (!file.exists()) 
-                file.createNewFile();
+            createFileIfNotExist(path);
 
             // Write to the file
             BufferedWriter bw = new BufferedWriter(new FileWriter(path, true));
@@ -52,11 +52,8 @@ public class FileManager {
     public static boolean checkUsernameExist(String username) {
         String path = "data/userData.txt";
         try {
-            File file = new File(path);   
-            
             // Create the file if it doesn't exist
-            if (!file.exists())
-                file.createNewFile();
+            createFileIfNotExist(path);
 
             // Read the file
             BufferedReader br = new BufferedReader(new FileReader(path));
@@ -80,11 +77,8 @@ public class FileManager {
     public static boolean checkUsernamePasswordMatch(String userData) {
         String path = "data/userData.txt";
         try {
-            File file = new File(path);   
-            
             // Create the file if it doesn't exist
-            if (!file.exists())
-                file.createNewFile();
+            createFileIfNotExist(path);
 
             // Read the file
             BufferedReader br = new BufferedReader(new FileReader(path));
