96484ba0925cd3525c34ab1bb4736818bfe8b50d
nguyenne <ajhaksdhjas@gmail.com>
2023-06-27 00:31:23 +0700

    add 10 movies default

--- src/main/java/util/DataInit.java
+++ src/main/java/util/DataInit.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.io.File;
+import java.util.ArrayList;
 
 
 public class DataInit {
@@ -11,9 +12,19 @@ public class DataInit {
             FileManager.createFileIfNotExist("data/movieData.txt");
         }
         // Enter 10 movies default to file
+        ArrayList<String> movieDefault = new ArrayList<>();
+        movieDefault.add("Spiderman\n");
+        movieDefault.add("Batmann\n");
+        movieDefault.add("Venom\n");
+        movieDefault.add("Superman\n");
+        movieDefault.add("Cô dâu 8 tuổi\n");
+        movieDefault.add("Thám tử Conan\n");
+        movieDefault.add("The forest\n");
+        movieDefault.add("Joker\n");
+        movieDefault.add("Rambo\n");
+        movieDefault.add("Thợ săn kho báu");
         for (int i = 0; i < 10; i++) {
-            String enterMovie = InputManager.inputString("Enter" + i + "movie");
-            FileManager.writeToFile("data/movieData.txt", enterMovie + "\n");
+            FileManager.writeToFile("data/movieData.txt", movieDefault.get(i));
         }
     }
     // add movie when there are already 10 default movies
@@ -27,8 +38,8 @@ public class DataInit {
             // Add movie to file
             String addMovie = InputManager.inputString("Enter a movie");
             // If admin enters "Quit", the program will quit.
-            // Note equalsIgnorCase checks the string entered by admin, it doesnot interest uppercase or lowercase
-            // Quit --> true, Dragonball --> false
+            // Note eualsIgnorCase checks the string entered by admin, it doesnot interest uppercase or lowercase.
+            // Quit --> true, Dragonball --> false.q
             if (addMovie.equalsIgnoreCase("Quit")) {
                 break;
             }
