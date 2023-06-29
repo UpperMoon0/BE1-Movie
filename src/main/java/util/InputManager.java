841acffd1fb8f0838d5aef84435b910d5e00e2fb
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-06-29 12:26:52 +0700

    buy ticket part 3 - done

--- src/main/java/util/InputManager.java
+++ src/main/java/util/InputManager.java
@@ -32,14 +32,15 @@ public class InputManager {
 
         do {
             inputStr = inputString("Enter your username:");
-            if (!inputStr.matches("^[A-Za-z]\\d{1,}[A-Za-z0-9]{2,10}$")) {
+            if (!inputStr.matches("^[a-zA-Z][a-zA-Z0-9]{3,11}$")) {
                 System.out.println("Username must:");
                 System.out.println("- Start with a letter");
                 System.out.println("- Contain at least 1 digit");
                 System.out.println("- Length from 4 to 12 characters");
                 System.out.println("- Can't contain any special characters");
+                System.out.println("Please try again!");
             }
-        } while (!inputStr.matches("^[A-Za-z]\\d{1,}[A-Za-z0-9]{4,12}$"));
+        } while (!inputStr.matches("^[a-zA-Z][a-zA-Z0-9]{3,11}$"));
 
         return inputStr;
     }
@@ -50,8 +51,13 @@ public class InputManager {
 
         do {
             inputStr = inputString("Enter your password:");
-            if (!inputStr.matches("(?=.*\\d)(?=.*[A-Z])[a-zA-Z0-9]{1,12}"))
-                System.out.println("Password must contain at least 1 digit and 1 uppercase letter, without any special characters, and can't be longer than 12 characters, please try again!");
+            if (!inputStr.matches("(?=.*\\d)(?=.*[A-Z])[a-zA-Z0-9]{1,12}")) {
+                System.out.println("Password must:");
+                System.out.println("- Contain at least 1 digit and 1 uppercase letter");
+                System.out.println("- Without any special characters,");
+                System.out.println("- Can't be longer than 12 characters");
+                System.out.println("Please try again!");
+            }
         } while (!inputStr.matches("(?=.*\\d)(?=.*[A-Z])[a-zA-Z0-9]{1,12}"));
 
         return inputStr;
@@ -62,17 +68,14 @@ public class InputManager {
         // Get movies list
         List<Movie> moviesList = new ArrayList<Movie>(); 
         List<String> movieData = FileManager.readEveryLines("data/movieData.txt");
-        for (String movie : movieData) {
-            if (movie != null) {
-                String[] parts = movie.split(",");
-                moviesList.add(new Movie(parts[0], Integer.parseInt(parts[1])));
-            }
-        }
 
-        // Show movies list
         System.out.println("Choose a movie:");
-        for (int i = 0; i < moviesList.size(); i++) {
-            System.out.println((i + 1) + ". " + moviesList.get(i));
+        for (int i = 0; i < movieData.size(); i++) {
+            if (movieData.get(i) != null) {
+                String[] parts = movieData.get(i).split(",");
+                moviesList.add(new Movie(parts[0], Integer.parseInt(parts[1])));
+                System.out.println((i + 1) + ". " + parts[0] + ", price: " + parts[1] + " VND");
+            }
         }
 
         // Get user input
@@ -82,7 +85,7 @@ public class InputManager {
         do {
             inputInt = UI.sc.nextInt();
             if (inputInt > 0 && inputInt <= moviesList.size())
-                return moviesList.get(inputInt - 1).getName() + ", price: " + moviesList.get(inputInt - 1).getPrice() + " VND";
+                return moviesList.get(inputInt - 1).getName();
             else
                 System.out.println("Invalid choice, please try again!");
         } while (true);
